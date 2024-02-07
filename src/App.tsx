ec99bde54f45cbab99084ea755054dbae1686a6f
Flow-Fly <florian.aube@gmail.com>
2024-02-07 10:15:36 +0100

    useState setter function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react"
 import "./App.css"
 import Button from "./components/Button"
 
 function App() {
+	const [count, setCount] = useState(0)
 	return (
 		<>
-			<Button>Click me!</Button>
+			<Button onClick={setCount}>Click me!</Button>
 		</>
 	)
 }
