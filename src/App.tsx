9d94cd38db57a930aa21ca22bed2f3dda657d326
Flow-Fly <florian.aube@gmail.com>
2024-02-07 10:51:05 +0100

    as const

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css"
+import { useState } from "react"
 import Button from "./components/Button"
 
+type User = {
+	_id: string
+	username: string
+	age: number
+}
+
 function App() {
+	const [user, setUser] = useState<User | null>(null)
+
+	const names = ["Bob", "John", "Alice"] as const
+	names[0] = "bob"
 	return (
 		<>
 			<Button />
