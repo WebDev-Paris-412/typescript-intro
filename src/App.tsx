74f7e90c10db9ddca99eb4a1327949a69768ef19
Flow-Fly <florian.aube@gmail.com>
2024-02-07 09:53:19 +0100

    tuple and array

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import Button from "./components/Button"
 function App() {
 	return (
 		<>
-			<Button backgroundColor={"green"} textColor="blue" pillShape={true} />
+			<Button
+				backgroundColor={"green"}
+				textColor="blue"
+				pillShape={true}
+				padding={[15, 20, "px", 56]}
+			/>
 		</>
 	)
 }
