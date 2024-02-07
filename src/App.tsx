64c06c7a566fea103ac6124e40892b7eb1f305dc
Flow-Fly <florian.aube@gmail.com>
2024-02-07 09:25:33 +0100

    Typing variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import { useState } from "react"
 import "./App.css"
+
+const url: string = "5"
+console.log(url)
+// Type is not needed but usually infered
+let otherUrl = "http://somewhere"
+
+otherUrl = 5
 
 function App() {
 	return <></>
